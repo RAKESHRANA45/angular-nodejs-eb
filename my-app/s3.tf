d99c8cf32acd0a940b53b5556a4e2bea7882885a
RAKESHRANA45 <65328021+RAKESHRANA45@users.noreply.github.com>
2022-03-29 19:33:30 +0530

    Update s3.tf

--- my-app/s3.tf
+++ my-app/s3.tf
@@ -32,29 +32,3 @@ resource "aws_s3_bucket" "root_bucket" {
 
   tags = var.common_tags
 }
-# data "aws_iam_policy_document" "allow_access_from_another_account" {
-#   statement {
-#     principals {
-#       type        = "AWS"
-#       identifiers = ["*"]
-#     }
-
-#     actions = [
-#       "s3:GetObject",
-#       "s3:ListBucket",
-#     ]
-
-#     resources = [
-#       aws_s3_bucket.root_bucket.arn,
-#       "${aws_s3_bucket.www_bucket.arn}/*",
-#     ]
-#   }
-# }
-# resource "aws_s3_bucket_policy" "allow_access_from_another_account1" {
-#   bucket = aws_s3_bucket.root_bucket.id
-#   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-# }
-# resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-#   bucket = aws_s3_bucket.www_bucket.id
-#   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-# }
